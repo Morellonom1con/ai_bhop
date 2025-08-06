20b0a77968ffc6b35760690f3c64e85445b0d22c
Morellonom1con <bhargavsadari32@gmail.com>
2025-08-06 13:46:43 +0530

    just wanted it saved

--- bhop.py
+++ bhop.py
@@ -10,21 +10,30 @@ def air_move(fmove,smove):
     pass
 
 pygame.init()
-screen=pygame.display.set_mode((400,400))
+screen=pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("bhop simulation")
 clock=pygame.time.Clock()
 player_x=200
 player_y=200
 player_surface=pygame.image.load('player.png').convert_alpha()
 player_rect=player_surface.get_rect(center=(player_x,player_y))
-bg_surface=pygame.Surface((400,400))
+bg_surface=pygame.Surface((1000,1000))
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
-    player_x+=1
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_y-=1
+    if keys[pygame.K_s]:
+        player_y+=1
+    if keys[pygame.K_a]:
+        player_x-=1
+    if keys[pygame.K_d]:
+        player_x+=1
+    player_rect=player_surface.get_rect(center=(player_x,player_y))
     screen.blit(bg_surface,(0,0))
     screen.blit(player_surface,player_rect)
     pygame.display.update()
